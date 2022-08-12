955c3702360a4c66f6028f3e5d665b2c9773fafa
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-12 10:36:12 +0530

    Add files via upload

---
+++ Check_Regular.java
@@ -0,0 +1,39 @@
+/*
+Write a java program to accept two file name from user and check whether that file is regular or not.
+*/
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class CheckFile
+{
+    public void Check_Regular(String FileName)
+    {
+        File fobj = new File(FileName);
+
+        if(fobj.isFile() == true)
+        {
+            System.out.println("The file is regular...");
+        }
+        else
+        {
+            System.out.println("File does not exist or it is not regular");
+        }
+    }
+}
+
+
+class Check_Regular
+{
+    public static void main(String args[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the first file name : ");
+        String str = sobj.nextLine();
+
+        CheckFile cobj = new CheckFile();
+        cobj.Check_Regular(str);
+    }
+}
