67d29308766c5da4d19f4d7287d24ec83e0d112a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-08-12 10:25:35 +0530

    Add files via upload

---
+++ Copy_Data.java
@@ -0,0 +1,57 @@
+/*
+Write a java program to accept two file names from user and open first file and create new file(Second file) and copy the data from first file into newly created file.
+*/
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Copy
+{
+    public void Copy_Data(String src, String dest)
+    {
+        try
+        {
+            File fobj = new File(dest);
+
+            if(fobj.createNewFile() == true)
+            {
+                System.out.println("Destination file created succesfully : "+fobj.getName());
+            }
+
+            FileInputStream finobj = new FileInputStream(src);
+            FileOutputStream foutobj = new FileOutputStream(dest);
+
+            int i = 0;
+
+            while((i = finobj.read()) != -1)
+            {
+                foutobj .write(i);
+            }
+
+            System.out.println("Copying finished...");
+        }
+        catch(IOException obj)
+        {
+            System.out.println(obj);
+        }
+    }
+}
+
+
+class Copy_Data
+{
+    public static void main(String args[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the first file name : ");
+        String str1 = sobj.nextLine();
+
+        System.out.println("Enter the second file name : ");
+        String str2 = sobj.nextLine();
+
+        Copy cobj = new Copy();
+        cobj.Copy_Data(str1, str2);
+    }
+}
